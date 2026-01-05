75e51169d4dce85862abe22fc9f1ff9ba16e01bc
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-05 17:50:30 +0530

    const Calection of student

--- first.js
+++ first.js
@@ -6,11 +6,19 @@ console.log(number);
 FullName = 34;
 console.log(FullName);
 
-age = 22;
-console.log(age);
+// age = 22;
+// console.log(age);
 
-age = "Rahul";
-console.log(age);
+// age = "Rahul";
+// console.log(age);
 
-const age = "Rahul";
-console.log(age);
+// const age = "Rahul";
+// console.log(age);
+
+// calection of sttudent
+const student ={
+    fullName : "Rahul Suthar",
+    cgpa : 9.1,
+    age : 20,
+    isPass : true
+}

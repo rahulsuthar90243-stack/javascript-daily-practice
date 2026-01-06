ae4c9f86e85898de099ef2d7dd84c01b712ac918
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-06 17:01:07 +0530

    const oject colled profile

---
+++ variables.js/Qs 2.js
@@ -0,0 +1,9 @@
+// // cont object colled "profile" to store information
+const profile = {
+    username : "@Rahul_Suthar",
+    isFollo : true,
+    followers : 431,
+    following : 167,
+};
+
+console.log(typeof profile["isFollo"]);

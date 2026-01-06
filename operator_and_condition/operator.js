460dc0ec6647a4f2a04e3b59aae9b04479b65432
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-06 18:19:30 +0530

    Assignment Operators practice in js

--- operator_and_condition/operator.js
+++ operator_and_condition/operator.js
@@ -6,3 +6,18 @@ console.log("Subtraction:", a - b); // 0
 console.log("Multiplication:", a * b); // 100
 console.log("Division:", a / b); // 1
 console.log("Modulus:", a % b); // 0
+
+// Assignment Operators
+let c = 5;
+c = 10; // Assignment
+console.log("Assignment:",c ); // 10
+c += 5;
+console.log("Addition Assignment:", c); // 15
+c -= 3;
+console.log("Subtraction Assignment:", c); // 12
+c *= 2;
+console.log("Multiplication Assignment:", c); // 24
+c /= 4;
+console.log("Division Assignment:", c); // 6
+c %= 4;
+console.log("Modulus Assignment:", c); // 2

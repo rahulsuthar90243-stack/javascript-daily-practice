7dbdb79ef42c6bf2ac2e975a60b29854b85762fc
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-06 18:11:41 +0530

    Arithmetic Operators prectice

---
+++ operator_and_condition/operator.js
@@ -0,0 +1,8 @@
+// Arithmetic Operators
+let a = 10;
+let b = 10;
+console.log("Addition:", a + b); // 20
+console.log("Subtraction:", a - b); // 0
+console.log("Multiplication:", a * b); // 100
+console.log("Division:", a / b); // 1
+console.log("Modulus:", a % b); // 0

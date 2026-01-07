026f76e5c319b91e53cdc12154395abca3fece8a
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-07 15:32:38 +0530

    for loop practice

---
+++ loop & string/for_loop.js
@@ -0,0 +1,21 @@
+// for loop
+
+let name = "Rahul";
+for (let i = 0; i < 5; i++){
+    console.log(name);
+}
+
+for(let i = 1; i <= 10; i++){
+    console.log("i = ", i);
+}
+
+let sum = 0;
+for(let i = 1; i <= 5; i++){
+    sum += i;
+}
+
+console.log("sum = ", sum);
+
+for(let i = 5; i >= 1; i--){
+    console.log("i = ", i);
+}

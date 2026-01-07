a3dae334ae1ff6a699cc2a56d23a9be6e1a6fda2
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-07 16:46:07 +0530

    for-of-loop and for-in-loop practice in js code

--- loop & string/for_loop.js
+++ loop & string/for_loop.js
@@ -19,3 +19,33 @@ console.log("sum = ", sum);
 for(let i = 5; i >= 1; i--){
     console.log("i = ", i);
 }
+
+
+// for - of - of loop
+let myname = "Rahul";
+let size = 0;
+for(let char of myname){
+    console.log(char);
+    size++;
+}
+
+console.log("size of string is :", size);
+
+// for - in - of loop
+let students = {
+    name: "Rahul",
+    age: 20,
+    course: "BCA",
+    collage: "SP Collage Sirohi",
+};
+
+for(let key in students){
+    console.log("key is: ", key , "and value is: ", students[key]);;
+}
+
+let s = 0;
+for(let key in students){
+    s++;
+}
+
+console.log("size of object is: ", s);

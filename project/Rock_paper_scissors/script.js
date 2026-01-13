b20f663d3982b2de109d4c489d70d6d243770301
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-13 10:39:15 +0530

    play game logic drowGame and show Winner add with funtion

--- project/Rock_paper_scissors/script.js
+++ project/Rock_paper_scissors/script.js
@@ -5,16 +5,43 @@ let comp_score = 0;
 const choices = document.querySelectorAll(".choice");
 
 const genCompChoice = () => {
- const option = ["Rock", "Papaer", "Scissors"];
+ const option = ["rock", "paper", "scissors"];
  const randIdx = Math.floor(Math.random()*3);
  return option[randIdx];
 }
 
+const drowGame = () =>{
+    console.log("Drow Game!");
+}
+
+
+const showWinner = (userWin)  => {
+    if(userWin){
+        console.log("You win");
+    }else{
+        console.log("you loss");
+    }
+}
+
 const playGame = (user_choice) =>{
-    console.log(user_choice);
+    console.log("user choice = ", user_choice);
 
     const compChoice = genCompChoice();
     console.log("comp choice = ", compChoice);
+
+    if(user_choice === compChoice){       //drow Game
+     drowGame();
+    }else{
+        let userWin = true;
+        if(user_choice === "rock"){
+            userWin = compChoice === "paper" ? false : true;
+        }else if(user_choice == "paper"){
+            userWin = compChoice === "scissors" ? false : true;
+        }else{
+            userWin = compChoice === "rock" ? false : true;
+        }
+        showWinner(userWin);
+    }
 }
 
 choices.forEach(choice =>{

a959d7eb215b0df5b6be86996b4314e45da64cf1
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-13 10:06:53 +0530

    generate compter choice and call in playGame

--- project/Rock_paper_scissors/script.js
+++ project/Rock_paper_scissors/script.js
@@ -4,8 +4,17 @@ let comp_score = 0;
 
 const choices = document.querySelectorAll(".choice");
 
+const genCompChoice = () => {
+ const option = ["Rock", "Papaer", "Scissors"];
+ const randIdx = Math.floor(Math.random()*3);
+ return option[randIdx];
+}
+
 const playGame = (user_choice) =>{
     console.log(user_choice);
+
+    const compChoice = genCompChoice();
+    console.log("comp choice = ", compChoice);
 }
 
 choices.forEach(choice =>{

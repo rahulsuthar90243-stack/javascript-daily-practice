2a11a4d1cdfe405fd2a1709d4b30ed4f639854eb
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-14 23:01:22 +0530

    nested callback practice to understand

---
+++ callback/callback_hell.js
@@ -0,0 +1,25 @@
+function getData(dataid, getNextData){
+    
+    setTimeout( () =>{
+        console.log("Data",dataid );
+        if(getNextData){
+        getNextData();
+        }
+    }, 2000);
+   
+}
+
+
+
+getData(1, () =>{
+    console.log("getting data2....");
+    getData(2, ()=>{
+        console.log("getting data3....");
+        getData(3, ()=>{
+            console.log("getting data4....");
+           getData(4, ()=>{
+            getData(5);
+      });   
+    });
+  });
+});

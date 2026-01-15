571c5d9a2570b541ed3565ce66d7dd65b79571d8
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-15 19:03:41 +0530

    Add async/await implementation for sequential data fetching

---
+++ Promises/async-await-data.js
@@ -0,0 +1,69 @@
+function getData(dataid){
+   return new Promise ((resolce, reject) =>{
+    setTimeout( () =>{
+        console.log("Data",dataid );
+        resolce("successfull data resolve");
+    }, 2000);
+   });
+};
+
+
+// Async-await
+
+async function getAllData(){
+    console.log("getting data1...");
+    await getData(1);
+    console.log("getting data2...");
+    await getData(2);
+    console.log("getting data3...");
+    await getData(3);
+    console.log("getting data4...");
+    await getData(4);
+    console.log("getting data5...");
+    await getData(5);
+
+}
+
+
+// LLFE
+// function 1.
+// (async function(){
+//     console.log("getting data1...");
+//     await getData(1);
+//     console.log("getting data2...");
+//     await getData(2);
+//     console.log("getting data3...");
+//     await getData(3);
+//     console.log("getting data4...");
+//     await getData(4);
+//     console.log("getting data5...");
+//     await getData(5);
+
+// })();
+
+
+// function 2.
+// (async() =>{
+//     console.log("getting data1...");
+//     await getData(1);
+//     console.log("getting data2...");
+//     await getData(2);
+//     console.log("getting data3...");
+//     await getData(3);
+//     console.log("getting data4...");
+//     await getData(4);
+//     console.log("getting data5...");
+//     await getData(5);
+// })();
+
+
+// getData(1)
+//  .then((res) =>{
+//     console.log(res);
+//     return getData(2);
+//  }).then((res) =>{
+//     console.log(res);
+//     return getData(3);
+//  }).then((res) =>{
+//     console.log(res);
+//  })

6c9cb789db79a820c51332b6638792b6edd60fb1
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-01-21 16:55:28 +0530

    find factorial with page ul

--- loop_string/factorial.js
+++ loop_string/factorial.js
@@ -1,8 +1,15 @@
 const num = prompt("Please, enter number of factorial.");
+let text = document.querySelector("#text");
+let result = document.querySelector("#result b");
+
 let fact = 1;
 
 for(let i = 1; i <= num; i++){
     fact *= i;
 }
 
-console.log(`Number ${num} is Factorial ${fact}`);
+text.innerText = `Your number is: ${num}`;
+
+result.innerText = `Number ${num} is Factorial ${fact}`
+
+// console.log(`Number ${num} is Factorial ${fact}`);

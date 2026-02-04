cd3f7b6081e938ef4ce445b81d8a7fe8adcbba4a
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-02-04 10:07:03 +0530

    exercise forEach loop with array

---
+++ array_and_loop/forEach loop_exercise/forEach.js
@@ -0,0 +1,17 @@
+// forEach
+
+const myCoding = ["js", "c", "c++", "Java", "python"];
+
+myCoding.forEach((item)=>{
+    console.log(item);
+})
+console.log("\n");
+
+myCoding.forEach((item, index, arr)=>{
+    console.log(item, index, arr);
+})
+
+// for(const val of myCoding){
+//     console.log(val);
+// }
+console.log("\n");

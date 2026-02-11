e1b8b2c34ff6bf2481b18a8658fa6fbb3384f2b9
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-02-11 16:45:08 +0530

    practice callback login and create zameto application

---
+++ callback/zameto application/callback.js
@@ -0,0 +1,56 @@
+// zomato application
+
+const orderDetail = {
+    orderId: 12003,
+    food: ["Pizza", "coke", "biryani", "bakhadi"],
+    cost: 1500,
+    customer: "Rahul",
+    customer_location: "baag colony",
+    restaurant_location: "jaipur"
+}
+
+
+function placedOrder(callback){
+    console.log("Payment is in progress");
+
+    setTimeout(()=>{
+        console.log("Payment is received and order get placed");
+        callback();
+    }, 3000)
+}
+
+function preparingOrder(callback){
+    console.log("Your food preparation started");
+
+    setTimeout(()=>{
+        console.log("Your order is now preparad");
+        callback();
+    }, 3000)
+}
+
+
+function pickupOrder(callback){
+    console.log("Delivery boy is on way to pickup order");
+
+    setTimeout(()=>{
+        console.log("I have pickup the order");
+        callback();
+    }, 3000)
+}
+
+function deliverOrder(){
+    console.log("I am on my way to deliver order");
+
+    setTimeout(()=>{
+        console.log("Order delivered succesfully");
+    }, 3000)
+}
+
+
+placedOrder(()=>{
+    preparingOrder(()=>{
+        pickupOrder(()=>{
+            deliverOrder();
+        });
+    });
+});

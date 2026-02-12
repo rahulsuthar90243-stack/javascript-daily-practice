d17a06840a536b678d8131181288a8cb11ab9b02
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-02-12 21:22:13 +0530

    callback practice in js

--- callback/zameto application/callback.js
+++ callback/zameto application/callback.js
@@ -54,3 +54,53 @@ placedOrder(()=>{
         });
     });
 });
+
+
+function checkInventory(callback){
+    setTimeout(()=>{
+    console.log("checking the inventory...");
+    callback();
+    }, 2000)
+}
+
+function createOrder(callback){
+    setTimeout(()=>{
+        console.log("create order...");
+        callback();
+    }, 1000)
+}
+
+function chargePayment(callbcak){
+    setTimeout(()=>{
+        console.log("charging the payment...");
+        callbcak();
+    }, 2000)
+}
+
+function sendInverntory(callback){
+    setTimeout(()=>{
+        console.log("send inverntory...");
+        callback();
+    }, 1000);
+}
+
+function main(){
+    checkInventory(()=>{
+        createOrder(()=>{
+            chargePayment(()=>{
+                sendInverntory(()=>{
+                    console.log('All done.');
+                });
+            });
+        });
+    });
+            
+console.log("Outher request processing");
+}
+
+main();
+// checkInventory();
+// createOrder();
+// chargePayment();
+// sendInverntory();
+// console.log("All tast conplete.");

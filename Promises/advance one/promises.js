2e0863389073285bf12bd30037222a272e258204
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-02-13 14:43:46 +0530

    Practice Promises: log async task and completion using then()

--- Promises/advance one/promises.js
+++ Promises/advance one/promises.js
@@ -1,11 +1,23 @@
 const promiseOne = new Promise((resolve, reject)=>{
     setTimeout(()=>{
-        console.log("Asysc task is compelete");
+        console.log("Async task is compelete");
         resolve();
-    }, 2000)
+    }, 1000)
 })
 
 
 promiseOne.then((rev)=>{
     console.log("Promise consumed");
+})
+
+
+
+new Promise (function(resolve, reject){
+    setTimeout(function(){
+    console.log("Async task 2 ");
+    resolve()
+    }, 2000)
+    
+}).then(function (){
+    console.log("Async task 2 compelete");
 })

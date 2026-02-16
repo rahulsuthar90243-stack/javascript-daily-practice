9474611cc4bd6eeb627f00d8479c708785bc1825
Rahul Suthar <rahulsuthar90243@gmail.com>
2026-02-16 21:03:25 +0530

    find current age practice

---
+++ practice/p.js
@@ -0,0 +1,10 @@
+const currentYear = 2026;
+
+function ageCalculator(age){
+    let age1 = currentYear - age;
+    // console.log(age1)
+    return age1;
+}
+
+ let a = ageCalculator(2007)
+ console.log(a);

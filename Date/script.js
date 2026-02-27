78241c4f8c4a4767739dca8e07624f5813435af5
Rahul <rahulsuthar90243@gmail.com>
2026-02-27 20:30:46 +0530

    practice find Date with js

---
+++ Date/script.js
@@ -0,0 +1,16 @@
+// Date Object
+let date = new Date();
+console.log(date);
+
+// Get Full Year
+let year = date.getFullYear();
+console.log("Year:", year);
+
+// Get Times
+console.log("Hours:", date.getTime());
+
+//Get Day
+console.log("Day:", date.getDay());
+
+// Get Month
+console.log("Month:", date.getMonth() + 1); // Months are zero-based

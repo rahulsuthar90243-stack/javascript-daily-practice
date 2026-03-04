13da3dd7f8c2d9c1b5d1af348ecadfb5324019d4
Rahul <rahulsuthar90243@gmail.com>
2026-03-04 21:43:54 +0530

    JS Practice fibonacci Series

---
+++ practice/fibonacci.js
@@ -0,0 +1,9 @@
+function fibonacciSeries(n) {
+  const fib = [0, 1]; 
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2]; 
+  }
+  return fib.slice(0, n); 
+}
+
+console.log(fibonacciSeries(10)); 
